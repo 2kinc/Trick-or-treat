07022b5988629be411616162fc0106cbd3b427b4
Craftingexpert1 <dev.velp@gmail.com>
2019-05-05 21:29:32 -0400

    wow nicer leaderboards

--- index.js
+++ index.js
@@ -6,22 +6,25 @@
 
 //  Please, please, pleaaase do not copy this code. That is not nice.
 //  And plus, why are you looking at our code? Creeps.
+
+//Random functions first.
+//  beautify thing (to make the display of things more readable)
+function beautify(t) {var f=[{start:3,end:6,suffix:"K"},{start:6,end:9,suffix:"M"},{start:9,end:12,suffix:"B"},{start:12,end:15,suffix:"T"},{start:15,end:18,suffix:"q"},{start:18,end:21,suffix:"Q"},{start:21,end:24,suffix:"s"},{start:24,end:27,suffix:"S"},{start:27,end:30,suffix:"O"},{start:30,end:33,suffix:"N"},{start:33,end:36,suffix:"D"},{start:36,end:39,suffix:"uD"},{start:39,end:42,suffix:"dD"},{start:42,end:45,suffix:"tD"},{start:45,end:48,suffix:"qD"},{start:48,end:51,suffix:"QD"},{start:51,end:54,suffix:"sD"},{start:54,end:57,suffix:"SD"},{start:57,end:60,suffix:"oD"},{start:60,end:63,suffix:"nD"},{start:63,end:66,suffix:"V"},{start:66,end:69,suffix:"uV"},{start:69,end:72,suffix:"dV"},{start:72,end:75,suffix:"tV"},{start:75,end:78,suffix:"qV"},{start:78,end:81,suffix:"QV"},{start:81,end:84,suffix:"sV"},{start:84,end:87,suffix:"SV"},{start:87,end:90,suffix:"oV"},{start:90,end:93,suffix:"nV"},{start:93,end:96,suffix:"T"},{start:96,end:99,suffix:"uT"},{start:99,end:102,suffix:"dT"},{start:102,end:105,suffix:"tT"},{start:105,end:108,suffix:"qT"}].filter(f=>{var s=Math.log10(t);return s>=f.start&&s<f.end});if(f&&f.length){var s=t/Math.pow(10,f[0].start);return(s=(s=Math.floor(100*s)/100).toFixed(2))+f[0].suffix}return t};
+//  getter thing
+function $(thing) {
+    var el = document.querySelectorAll(thing);
+    el = (el.length == 1) ? el[0] : el.length == 0 ? null : el;
+    return el;
+}
+//...and then the game function!
 function Game(H,a,ll,o,w,ee,n) {
   var that = this;
-  //beautify thing (to make the display of things way easier)
-  function beautify(t) {var f=[{start:3,end:6,suffix:"K"},{start:6,end:9,suffix:"M"},{start:9,end:12,suffix:"B"},{start:12,end:15,suffix:"T"},{start:15,end:18,suffix:"q"},{start:18,end:21,suffix:"Q"},{start:21,end:24,suffix:"s"},{start:24,end:27,suffix:"S"},{start:27,end:30,suffix:"O"},{start:30,end:33,suffix:"N"},{start:33,end:36,suffix:"D"},{start:36,end:39,suffix:"uD"},{start:39,end:42,suffix:"dD"},{start:42,end:45,suffix:"tD"},{start:45,end:48,suffix:"qD"},{start:48,end:51,suffix:"QD"},{start:51,end:54,suffix:"sD"},{start:54,end:57,suffix:"SD"},{start:57,end:60,suffix:"oD"},{start:60,end:63,suffix:"nD"},{start:63,end:66,suffix:"V"},{start:66,end:69,suffix:"uV"},{start:69,end:72,suffix:"dV"},{start:72,end:75,suffix:"tV"},{start:75,end:78,suffix:"qV"},{start:78,end:81,suffix:"QV"},{start:81,end:84,suffix:"sV"},{start:84,end:87,suffix:"SV"},{start:87,end:90,suffix:"oV"},{start:90,end:93,suffix:"nV"},{start:93,end:96,suffix:"T"},{start:96,end:99,suffix:"uT"},{start:99,end:102,suffix:"dT"},{start:102,end:105,suffix:"tT"},{start:105,end:108,suffix:"qT"}].filter(f=>{var s=Math.log10(t);return s>=f.start&&s<f.end});if(f&&f.length){var s=t/Math.pow(10,f[0].start);return(s=(s=Math.floor(100*s)/100).toFixed(2))+f[0].suffix}return t};
-  //getter thing
-  function $(thing) {
-      var el = document.querySelectorAll(thing);
-      el = (el.length == 1) ? el[0] : el.length == 0 ? null : el;
-      return el;
-  }
   function rand(max, min) {min = min || 0;
     return Math.floor(Math.random()*max+min);
   }
   (function status(str) {
     $('#status').innerHTML = str;
-  }) ('Trick or treat!');
+  })('Trick or treat!');
   //If browser does not support local storage:
   if (!window.localStorage) {
     throw 'Hey! Get a better browser that supports local storage!';
@@ -53,16 +56,17 @@ function Game(H,a,ll,o,w,ee,n) {
     max_farmers:$('#max-farmers'),
     costumes:$('#multiplier'),
     max_costumes:$('#max-mult'),
+    cps:$('#cps'),
     tot_button:$('#tot-btn'),
     name:$('#name'),
-    newgame:$('#newgame'),
+    newgame:$('#new-game'),
     leaderboard:$('#ldrbrd-list'),
-  };this.ducks = {bestthinginworld:true};
-  this.readSaved = function() {
-    return JSON.parse(H.atob(localStorage.TrickOrTreat));
-  }
-  this.normalobj = {
-    candy: 13, // candy
+    shoptabs:$('.mdc-tab'),
+    shoppanels:$('.js-panel'),
+    increments:$('#increment_value')
+  };
+  this.normobj = { //make a default object
+    candy: 0, // candy
     max_candy: 70000, // max candy
     pump: 25, //pumpkins
     max_pump: 100, // max pumpkins
@@ -82,24 +86,40 @@ function Game(H,a,ll,o,w,ee,n) {
     maxCostumes: 200, // max costumes
     farmers: 0, // farmers
     maxFarmers: 100, //max farmers
-    name: Math.floor(Math.random() * 100000000000).toString(36), // name to display
+    name: Math.floor(Math.random() * 10 ** 11).toString(36), // name to display
     increments:[1,10,100,1000],
-  }
-  this.obj = this.readSaved() || this.normalobj;
-  this.save = function() {
-    localStorage.setItem('TrickOrTreat', H.btoa(JSON.stringify(this.obj)));
+    uid:'', // for firebase leaderboard
   };
+  this.readSave = function() {
+    var thing = JSON.parse(localStorage.getItem('TrickOrTreat'));
+    for (var spot in thing) {
+	     if (thing[spot] == 'Inf' && spot != 'name') {
+		       thing[spot] = Infinity;
+	     }
+    }
+    return thing;
+  };
+  this.obj = this.readSave() || this.normobj;
+  (this.save = function() { //save function (for saving, duh)
+    var thing = this.obj;
+    for (var item in thing) {
+	     if (thing[item] == Infinity && item != 'name') {
+		     thing[item] = 'Inf';
+	     }
+    };
+    localStorage.setItem('TrickOrTreat', JSON.stringify(thing));
+  })();
   this.newgame = function() {
-    localStorage.TrickOrTreat = undefined; //restart game
-    this.obj = this.normalobj;
+    localStorage.clear();
+    this.obj = this.normobj;
     auth.signOut().then(function() { //sign out
       console.log('Signed Out');
-    },function(error) { // if its an error
+    },function(error) { // if there is an error in signing out
       console.error('Sign Out Error:', error);
     });
     location.reload(); //and finally reload
   };
-  //this.elems.newgame.addEventListener('click',this.newgame);
+  this.elems.newgame.addEventListener('click', ()=>this.newgame());
   (this.updateElements = function() {
     that.elems.candy.innerText = beautify(that.obj.candy);
     that.elems.pumpkins.innerText = beautify(that.obj.pump);
@@ -112,32 +132,54 @@ function Game(H,a,ll,o,w,ee,n) {
     that.elems.farmers.innerText = 'Farmers: ' + beautify(that.obj.farmers);
     that.elems.max_farmers.innerText = 'Max Farmers: ' + beautify(that.obj.maxFarmers);
     that.elems.name.innerText = 'Name: ' + that.obj.name;
+    that.elems.cps.innerText = 'Candy Per Second: ' + beautify(that.obj.tot_ers * that.obj.costumes);
+    that.elems.increments;
   })();
+  this.elems.shoptabs.forEach(function(a, b) {
+    a.addEventListener('click', function() { //all of the shop tab things
+      that.elems.shoppanels.forEach(function(index, c) {
+        index.classList.remove("is-active");
+        if (b == c) {
+          index.classList.add('is-active');
+        }
+      });
+    });
+  });
   this.trickOrTreat = function() { // Trick or treat function
-    that.obj.candy += rand(that.obj.costumes,1);
+    var add = rand(that.obj.costumes,1);
+    that.obj.candy =
+      (that.obj.candy + add <= that.obj.max_candy) ?
+        that.obj.candy + add :
+        that.obj.max_candy;
     that.updateElements();
   };
   this.elems.tot_button.addEventListener('click',this.trickOrTreat);
   this.onesecond = setInterval(function(){ //Runs every second
     that.updateElements();
-    that.save();
     var add = rand(that.obj.costumes,1) * that.obj.tot_ers; //assign variable to add to candy based on tot_ers
     that.obj.candy =
       (that.obj.candy + add <= that.obj.max_candy) ? //if the sum is less than or equal to max amount of candy
         that.obj.candy + add : //continue onward
         that.obj.max_candy;// otherwise make the candy that max amount
   },1000);
-  this.shsecond = setInterval(function(){ //Runs every seven and a half second (farmers do this)
+  this.ftsecond = setInterval(function(){ //Runs every 15th second (farmers need this)
     var add = Math.floor(rand(2,1) * that.obj.farmers / 4); //assign variable to add to pumpkins based on farmers
     that.obj.pump = //heh i copied this from onesecond interval
       (that.obj.pump + add <= that.obj.max_pump) ? //if the sum is less than or equal to max amount of pumpkins
         that.obj.pump + add : //continue onward
         that.obj.max_pump;// otherwise make the pumpkins that max amount
-  },7500);
+  },1500);
   H.onunload = function () {
     that.save();
   }
-}
+};
+//And then make the game!
+var game = new Game(window);
+
+//THAT up there was like the more important stuff
+
+
+// And then the Firebase stuff
 var config = {
     apiKey: "AIzaSyADD6YWKrzibRMwJNi1FwUR0jcR0GitZPI",
     authDomain: "k-inc-232222.firebaseapp.com",
@@ -149,25 +191,73 @@ var config = {
 var app = firebase.initializeApp(config);
 var database = app.database().ref().child('tot');
 var auth = app.auth();
-var game = new Game(window);
+
 game.elems.signinbtn.addEventListener('click', function() { // firebase sign in
-  auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-});//leaderboard reset maybe!!!
-database.orderByChild('candy').on('child_changed', function (snapshot) { //vodka rules
-  var obj = snapshot.val();
+  if (!auth.currentUser) {
+    auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  } else {
+    auth.signOut().then(function() { //sign out
+      console.log('Signed Out');
+    },function(error) { // if there is an error in signing out
+      console.error('Sign Out Error:', error);
+    });
+  }
+});
+
+function addToLeaderboard(name,candy,pumpkins,cps,id) {
   var element = document.createElement('li');
-  game.elems.leaderboard.prepend(element);
-  element.outerHTML =`<li class="mdc-list-item"><span class="mdc-list-item__text" style="width:25%">` + d.name + `</span>
-  <span class="mdc-list-item__text" style="width:25%">` + beautify(d.candy) + `</span>
-  <span class="mdc-list-item__text" style="width:25%">` + beautify(d.pump) + `</span>
-  <span class="mdc-list-item__text" style="width:25%">` + beautify(d.tot_ers * d.costumes) + `</span></li>`;
-}); // whenever the leaderboard changes. Can we make leaderboard reset
+  game.elems.leaderboard.prepend(element); // prepend it because it does the ordering in reverse
+  element.outerHTML =`
+  <li class="mdc-list-item" id="` + id + `">
+    <span class="mdc-list-item__text" style="width:25%">` + name + `</span>
+    <span class="mdc-list-item__text" style="width:25%">` + candy + `</span>
+    <span class="mdc-list-item__text" style="width:25%">` + pumpkins + `</span>
+    <span class="mdc-list-item__text" style="width:25%">` + cps + `</span>
+  </li>`;
+};
+function setToLeaderboard(name,candy,pumpkins,cps,id) {
+  $('#'+id).outerHTML =`
+  <li class="mdc-list-item" id="` + id + `">
+    <span class="mdc-list-item__text" style="width:25%">` + name + `</span>
+    <span class="mdc-list-item__text" style="width:25%">` + candy + `</span>
+    <span class="mdc-list-item__text" style="width:25%">` + pumpkins + `</span>
+    <span class="mdc-list-item__text" style="width:25%">` + cps + `</span>
+  </li>`;
+};
+function removeFromLeaderboard(id) {
+  var el = $('#'+id);
+  el.parentElement.removeChild(el);
+}
+// Another one second interval for firebase things
+setInterval(function() {
+  if (auth.currentUser) {
+    game.obj.uid = auth.currentUser.uid;
+    database.child(auth.currentUser.uid).set(game.obj);
+  }
+  game.elems.leaderboard.firstElementChild.classList.add('first-place'); // make the person in first place special
+},1000);
+database.orderByChild('candy').on('child_added', function (snapshot) {
+  var obj = snapshot.val();
+  addToLeaderboard(obj.name,beautify(obj.candy),beautify(obj.pump),beautify(obj.tot_ers * obj.costumes), snapshot.key);
+});
+database.orderByChild('candy').on('child_changed', function (snapshot) {
+  var obj = snapshot.val();
+  setToLeaderboard(obj.name,beautify(obj.candy),beautify(obj.pump),beautify(obj.tot_ers * obj.costumes), snapshot.key);
+});
+database.orderByChild('candy').on('child_removed', function (snapshot) {
+  removeFromLeaderboard(snapshot.key);
+});
+// On sign in or sign out
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       database.child(auth.currentUser.uid).once('value').then(function(snapshot) {
         game.obj = snapshot.val();
-      });console.log('yuh mamie math calculations ham.')
+      });
+      console.log('yuh mamie math calculations ham.'); // i don't know why i did this
+      game.obj.uid = auth.currentUser.uid;
+      game.elems.signinbtn.innerText = 'Sign out to not be on the leaderboard!';
     } else {
         console.log('Sign in to save your progress!');
+        game.elems.signinbtn.innerText = 'Sign in to be on the leaderboard!';
     }
 });
